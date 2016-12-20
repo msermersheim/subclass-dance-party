f92f781def68952eb6d910122bedffb35d452ee8
Daniel Voigt <danielralphvoigt@gmail.com>
2016-12-19 21:55:06 -0800

    Pass all tests

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -28,6 +28,7 @@ describe('blinkyDancer', function() {
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
+
       expect(blinkyDancer.step.callCount).to.be.equal(2);
     });
   });
